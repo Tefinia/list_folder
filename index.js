18fc58a7aac4c3e75bad591775e3ec355643b3ac
Tefinia <tefiraza@gmail.com>
2023-11-29 10:19:43 +0300

    FolderFileDisplay

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
+const express = require('express');
 const fs = require('fs');
+const app = express();
+const port = 3000;
 
-// Chemin du répertoire que vous souhaitez lister
-const cheminDuRepertoire = '/home/tefi/Vidéos/';
+const fichiers = [ ];
 
-// Utiliser la fonction readdir pour lire le contenu du répertoire
-fs.readdir(cheminDuRepertoire, (erreur, fichiers) => {
-  if (erreur) {
-    console.error('Erreur lors de la lecture du répertoire :', erreur);
-    return;
-  }
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 
-  // Fichiers contient maintenant la liste des fichiers/dossiers
-  console.log('Contenu du répertoire :');
-  fichiers.forEach((fichier) => {
-    console.log(fichier);
+app.set('view engine', 'ejs');
+
+// affichage du chemin
+app.post('/path', (req, res) => {
+  let chemin = req.body.path;
+
+  fs.readdir(chemin, (erreur, fichiers) => {
+    if (erreur) {
+      msg = "Erreur lors de la lecture du répertoire";
+      fichiers = [ ];
+      res.render('list', {fichiers: fichiers});
+      return;
+    }
+    // Fichiers contient maintenant la liste des fichiers/dossiers
+    res.render('list', {fichiers, chemin:chemin});
   });
+  
+});
+
+app.get('/', (req, res) => {
+  res.render('list', {fichiers: fichiers});
+})
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur le port ${port}`);
 });
